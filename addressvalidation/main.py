42d402c54393a50f5d9f5d4c1a4af44d8e9d9c81
jonriklan <56042403+jonriklan@users.noreply.github.com>
2019-11-05 16:11:58 -0500

    Add files via upload

---
+++ addressvalidation/main.py
@@ -0,0 +1,58 @@
+import sys
+import geocoder
+import time
+import pandas as pd
+import numpy as np
+def dogeocode():
+        array =[]
+        t = open('notvalid.txt','w')
+        q = open('valid.txt','w')
+        with open("addresses.txt") as textFile:
+            list = textFile.readlines()
+            i=0
+            while i<len(list):
+                list[i]=list[i].strip('\n')
+                i+=1
+        listgood =[]
+        listbad = []
+        z =0
+
+        while z < len(list):
+            p=0
+            f = geocoder.locationiq(list[z],key='6774a6e0ff9f5f')
+            address = f.address
+            location = f.location
+            if location.find(" Blvd")!=-1:
+                location =location.replace(" Blvd", " Boulevard")
+            if location.find(" Rd")!=-1:
+                location =location.replace(" Rd", " Road")
+            if location.find(" Rt")!=-1:
+                location =location.replace(" Rt", " Route")
+            if location.find(" Hwy")!=-1:
+                location =location.replace(" Hwy", " Highway")
+            if location.find(" Pkway")!=-1:
+                location =location.replace(" Pkwy", " Parkway")
+            time.sleep(1)
+            y = 0
+            string = ""
+            while location[y] != ",":
+                string = string + location[y]
+
+                y= y + 1
+            if f.address.replace(',','').find(string)!=-1:
+                print(list[z] + " Is Valid")
+                listgood.append(list[z])
+                q.write(f.location+"\n")
+                q.write("\n")
+            else:
+                print(list[z] + " Is not Valid")
+
+                print(f.address)
+                listbad.append(list[z])
+                t.write("Current: " + f.location+"\n")
+                t.write("Closest: "+f.address+"\n")
+                t.write("\n")
+            z = z + 1
+        t.close()
+
+dogeocode()
